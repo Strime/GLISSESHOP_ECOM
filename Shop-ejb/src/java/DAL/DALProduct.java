d11f541239b5ec2acfa2231e12d48b880a3d9e35
strime <sancassg@f214-07.e-im2ag.ujf-grenoble.fr>
2014-09-17 16:02:46 +0200

    filtres trucs cools
    Conflicts:
    	Shop-ejb/src/java/Session/ProductSessionBean.java
    	Shop-war/src/java/webservices/ProductService.java
    	Shop-war/web/Scripts/account/controllers.js
    	Shop-war/web/Scripts/glisseAngular.js

--- Shop-ejb/src/java/DAL/DALProduct.java
+++ Shop-ejb/src/java/DAL/DALProduct.java
@@ -6,11 +6,15 @@
 
 package DAL;
 
+import Entity.Carac;
 import Entity.Product;
 import Session.ProductSessionBean.VMCaracT;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 import javax.persistence.EntityManager;
 
 /**
@@ -35,7 +39,32 @@ public class DALProduct {
     }
 
     public static List<Product> getFromFamilyFiltered(Integer famID, List<String> possValues, List<Integer> possTypes, EntityManager em) {
-        List<Product> arr_prod = (List<Product>)em.createQuery("SELECT DISTINCT c.referenceidReference.productidReference FROM Carac c WHERE c.typeCaracidCarac IN :types AND c.value IN :values").setParameter("types", possTypes).setParameter("values", possValues).getResultList();
+        List<Carac> arr_carac = (List<Carac>)em.createQuery("SELECT DISTINCT c FROM Carac c WHERE c.typeCaracidCarac IN :types AND c.value IN :values AND (c.referenceidReference.productidReference.famillyidProduct.idFamilly = "+famID.toString()+" OR (c.referenceidReference.productidReference.famillyidProduct.famillyidFamilly IS NOT NULL AND c.referenceidReference.productidReference.famillyidProduct.famillyidFamilly.idFamilly = "+famID.toString()+"))").setParameter("types", possTypes).setParameter("values", possValues).getResultList();
+        List<Product> arr_prod = new ArrayList<>();
+        
+        Map<Product,List<Integer>> candidates = new HashMap<>();
+        //pas super ça...
+        for(Carac c : arr_carac) {
+            Integer typeId = c.getTypeCaracidCarac().getIdTypeCarac();
+            Product product = c.getReferenceidReference().getProductidReference();
+            if(candidates.containsKey(product)) {
+                    if(!candidates.get(product).contains(typeId)) {
+                        candidates.get(product).add(typeId);
+                    }
+            }
+            else {
+                candidates.put(product, new ArrayList<Integer>());
+                candidates.get(product).add(typeId);
+            }
+        }
+        Iterator it = candidates.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pairs = (Map.Entry)it.next();
+            if((((List<Integer>) pairs.getValue()).size()==possTypes.size())) {
+                arr_prod.add((Product)pairs.getKey());
+            }
+                
+        }
         return arr_prod;
     }
 
